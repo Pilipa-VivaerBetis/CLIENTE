4b4d333bf42fdacca662d0f10f22abc0274912a8
Pilar <118005855+Pilipa500@users.noreply.github.com>
2023-11-26 13:35:00 +0100

    Casa26_nov

---
+++ Boletin 1/script.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName("boton1").addEventListener('click', function()){
+    //redirige a la página del primer ejercicio
+    window.location.href ="Ejercicio1Boletin1.html";
+}
+document.getElementsByClassName("boton2").addEventListener('click', function()){
+    //redirige a la página del primer ejercicio
+    window.location.href ="Ejercicio2Boletin1.html";
+}
+document.getElementsByClassName("boton3").addEventListener('click', function()){
+    //redirige a la página del primer ejercicio
+    window.location.href ="Ejercicio3Boletin1.html";
+}
+
