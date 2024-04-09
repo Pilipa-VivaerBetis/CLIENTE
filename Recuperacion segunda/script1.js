c507a0bac957c54b22d33f47880ba48e02ca68cd
Pilar <118005855+Pilipa500@users.noreply.github.com>
2024-04-09 17:45:04 +0200

    actualizado martes

--- Recuperacion segunda/script1.js
+++ Recuperacion segunda/script1.js
@@ -3,7 +3,7 @@ let carrito =[];
 
 document.addEventListener("DOMContentLoaded", function(){
  renderCards();
-//  renderBienes();
+renderCarrito();
 
  //const botonResetear =document.getElementById("boton-resetear");
 // botonResetear.addEventListener('click', botonVaciar);
@@ -32,7 +32,9 @@ function renderCards(){
 
         const uso = document.createElement("p");
         uso.textContent =bien.current_usage;
-        uso.addEventListener("click", addFiltro);
+        //uso.dataset.id=bien.id;//Añadir ID de bien como dataset para usar en el filtro
+        //uso.dataset.tipo = 'uso'; // Establecer tipo de filtro como 'uso'
+        uso.addEventListener("click", filtrarUso);
 
         cardBody.appendChild(title);
         cardBody.appendChild(subtitulo);
@@ -44,38 +46,82 @@ function renderCards(){
         itemsContainer.appendChild(div);
 
     });
-    renderCarrito();
+   renderCarrito();
 }
 
 /**
  * Funcion para que se muestren los filtros en el array carrito
  *  */
 function renderCarrito(){
-    const carritoContainer = document.getElementById('totales');
-    carritoContainer.innerHTML =" ";
+    const carritoContainer      = document.getElementById('totales');
+    carritoContainer.innerHTML  = " ";
 
-    const tituloFiltro=document.createElement("h1");
-    tituloFiltro.textContent="Filtros y totales";
+    const tituloFiltro          = document.createElement("h1");
+    tituloFiltro.textContent    = "Filtros y totales";
 
-    const subtituloFiltro=document.createElement("h3");
-    subtituloFiltro.textContent="Filtros rápidos";
-    subtituloFiltro.classList.add('lado-derecho');
+    const subtituloFiltro       = document.createElement("h3");
+    subtituloFiltro.textContent = "Filtros rápidos";
+    
+    carritoContainer.appendChild(tituloFiltro);
+    carritoContainer.appendChild(subtituloFiltro);
 
+    const contenedorLista = document.createElement("ul");
+    
     provincias.forEach(provincia=>{
-        const button = document.createElement("button");
-        button.textContent=provincia;
+        
+        // Crear elemento li
+        const listaProvincia= document.createElement("li");
+
+        // Crear elemento boton
+        const button        = document.createElement("button");
+        button.textContent  = provincia;
         button.addEventListener("click", filtrarProvincia);
 
+        // Crear elemento span
+        const conteo = document.createElement("span");
+        // Contar los bienes por provincia y actualizar el span
+        conteo.textContent = `(${bienes.filter(bien => bien.province === provincia).length})`;
+        // TODO
+
+        //Añadir boton y span al li
+        listaProvincia.appendChild(button);
+        listaProvincia.appendChild(conteo);
+
+        // Añadir el li al ul
+        contenedorLista.appendChild(listaProvincia);
     })
 
-    carritoContainer.appendChild (tituloFiltro);
-    carritoContainer.appendChild (subtituloFiltro);
-    carritoContainer.appendChild (provincia);
+    // Añadir ul al dom
+    carritoContainer.appendChild(contenedorLista);
+
+    // Creamos boton reset
+    const resetButton       = document.createElement("button");
+    resetButton.textContent = "Resetear filtros";
+    resetButton.addEventListener("click", botonVaciar);
+    
+    // Añadimos boton reset
+    carritoContainer.appendChild(resetButton);
+
+    //añado texto de filtro aplicado
+    const filtroAplicado =  document.createElement("p");
+    filtroAplicado.textContent = "Filtro aplicado:";
+    //sumatorio del total inmuebles o bienes
+    const totalInmuebles  = bienes.length;
+    //bienes es un numero tendré que hacerlo en html
+    //totalInmuebles
+    
+    
+    carritoContainer.appendChild(filtroAplicado);
+    //añadir total inmuebles 
+    carritoContainer.appendChild(totalInmuebles);
 
 }
 
-/**Función para filtrar provincias */
-function filtrarProvincia(){
+/**Función para filtrar provincias
+ * @param {string} provincia Nombre de la provincia seleccionada
+ */
+
+function filtrarProvincia(provincia){
 
 }
 
